9c478e2c722a9e44a2a0dc035b26887cf2971eb0
Jason Lockwood <JayLockwood@JLMacbookPro.home>
2016-07-08 22:04:40 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Jason Lockwood's Portfolio</title>
+        <meta name="description" content=""><!--Add site description here-->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="fonts.css" type="text/css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+
+
+
+
+
+        <!--BEGIN HTML SITE CODE-->
+
+        
+        
+        <div id="wrapper" class="clearfix">
+    		<div class="main-container">
+    			<header>
+      				<div id="logo">
+      					<a href="#">JASON LOCKWOOD
+						</a>
+      				</div>
+      				<div id="nav-container" class="clearfix">
+      					<div class="nav clearfix">
+      						<ul id="nav-menu">
+        						<li class="menu-item active"><a href="#">Works</a></li>
+        						<li class="menu-item"><a href="#">About</a></li>
+        						<li class="menu-item"><a href="#">Connect</a></li>
+      						</ul>
+      					</nav>	
+      				</div>	
+    			</header>
+
+    			<!-- END HEADER -->
+    			<!-- BEGIN WORK CONTENT -->
+			<ul id="content-list">
+				<li id="content-container" class="active">
+					<div id="content-area">
+						<section id="content">
+							<div id="content-inner">
+
+
+					<!--BEGIN POST -->
+
+								<div id="post-container">
+									<section class="post">
+										<div class="inner-post">
+											<a href="#">
+												<div class="post-image">
+													<img src="img/Unknown.jpeg">
+													<span class="post-text-wrapper">
+														<span class="post-text">
+															<p>A2ZLOGIX</p>
+																<span class="post-meta">Web Design & Development</span>
+														</span>
+													</span>
+												</div>
+											</a>
+										</div>
+									</section>
+								</div>
+
+					<!--END POST -->
+
+
+						<!--BEGIN POST -->
+
+								<div id="post-container">
+									<section class="post">
+										<div class="inner-post">
+											<a href="#">
+												<div class="post-image">
+													<img src="img/Unknown2.jpeg">
+													<span class="post-text-wrapper">
+														<span class="post-text">
+															<p>SKYFRAME</p>
+																<span class="post-meta">Web Design & Illustration</span>
+														</span>
+													</span>
+												</div>
+											</a>
+										</div>
+									</section>
+								</div>
+
+					<!--END POST -->
+
+
+						<!--BEGIN POST -->
+
+								<div id="post-container">
+									<section class="post">
+										<div class="inner-post">
+											<a href="#">
+												<div class="post-image">
+													<img src="img/Unknown3.jpeg">
+													<span class="post-text-wrapper">
+														<span class="post-text">
+															<p>GARDENHAUS</p>
+																<span class="post-meta">Branding</span>
+														</span>
+													</span>
+												</div>
+											</a>
+										</div>
+									</section>
+								</div>
+
+					<!--END POST -->
+
+
+						<!--BEGIN POST -->
+
+								<div id="post-container">
+									<section class="post">
+										<div class="inner-post">
+											<a href="#">
+												<div class="post-image">
+													<img src="img/Unknown4.jpeg">
+													<span class="post-text-wrapper">
+														<span class="post-text">
+															<p>COLEMAN</p>
+																<span class="post-meta">Branding</span>
+														</span>
+													</span>
+												</div>
+											</a>
+										</div>
+									</section>
+								</div>
+
+					<!--END POST -->
+
+								<a class="backtop" href="#content-container"><i class="fa fa-arrow-circle-up fa-3x"></i></a>
+							</div>
+						</section>
+					</div>
+				</li>
+
+
+
+				<!-- END WORK CONTENT -->
+
+
+				<!--BEGIN ABOUT CONTENT -->
+				<li id="content-container">
+					<div id="content-area">
+						<section id="content">
+							<div id="content-inner">
+
+
+					<!--BEGIN POST -->
+
+								<div id="connect-wrapper">
+									<h2>LITTLE FARTS</h2>
+								</div>
+							</div>
+						</section>
+					</div>
+				</li>
+
+				<!-- END CONTENT -->
+
+
+				<!--BEGIN CONNECT CONTENT -->
+				<li id="content-container">
+					<div id="content-area">
+						<section id="content">
+							<div id="content-inner">
+
+
+					<!--BEGIN FORM CONTENT -->
+								<div id="connect-wrapper">
+									<h2>CONNECT WITH ME</h2>
+
+									<ul id="errors" class="">
+										<li id="error-message"> Whoopsie whoops! Let's try this again!</li>
+									</ul>
+									<li id="success"> Success! Now we are talking!</li>
+
+									<form action="process.php" method="post">
+    									<div id="form-field">
+        									<label for="name"></label>
+        									<input type="text" id="name" placeholder="Full name" required="required" autofocus="autofocus" />
+        								</div>
+        								<div id="form-field">
+        									<label for="mail"></label>
+        									<input type="email" id="mail" placeholder="E-mail address" required="required"/>
+    									</div>
+    									<div id="form-field">
+        									<label for="msg"></label>
+        									<textarea id="msg" placeholder="Write your message here" required="required" data-minlength="20"></textarea>
+
+    									</div>
+    									<div class="button-wrap">
+        									<button class="fill" type="submit">Send message</button>
+    									</div>
+									</form>
+								</div>
+					<!--END FORM CONTENT-->
+
+							</div>
+						</section>
+					</div>
+				</li>
+			</ul>
+
+				<!-- END CONTENT -->
+
+
+				
+
+        		<footer class="clearfix">
+            		<div class="placeholder">
+                		<p>©Jason Lockwood Design 2015 | All Rights Reserved</p>
+            		</div>
+        		</footer>
+        	</div>
+        </div>
+
+ <script type="text/javascript">
+        $(function() {
+  			$('a[href*=#]:not([href=#])').click(function() {
+    		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      		var target = $(this.hash);
+      		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      		if (target.length) {
+       		 $('html,body').animate({
+          		scrollTop: target.offset().top
+        		}, 600);
+        		return false;
+    	  		}
+    		}
+  			});
+		});
+
+
+        $(document).ready(function(){
+    $("li.menu-item").click(function(e){
+        if (!$(this).hasClass("active")) {
+            var tabNum = $(this).index();
+            var nthChild = tabNum+1;
+            $("li.menu-item.active").removeClass("active");
+            $(this).addClass("active");
+            $("#content-container.active").removeClass("active");
+            $("#content-container:nth-child("+nthChild+")").addClass("active");
+        }
+    });
+});
+
+		</script>
+
+
+
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+    </body>
+</html>
